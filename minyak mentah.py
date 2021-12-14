bf13dd1e33df51a71add0681c8250589d48a5af2
Bagus Aulia Ahmad Fahrezi <95753543+BagusAulAF@users.noreply.github.com>
2021-12-14 18:55:16 +0700

    Add files via upload

---
+++ minyak mentah.py
@@ -0,0 +1,16 @@
+'''
+Aplikasi Analisis Statistik Minyak Mentah 
+
+Data diperoleh melalui CSV dan JSON
+Referensi API Streamlit: https://docs.streamlit.io/library/api-reference
+'''
+
+import numpy as np
+import streamlit as slit
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+############### title ###############
+slit.set_page_config(layout="wide")  
+############### title ###############
